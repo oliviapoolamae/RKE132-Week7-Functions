ee56b2a299990fe55352587e5d602f8e34cd8a6c
oliviapoolamae <olivia.poolamae@tktk.ee>
2024-10-16 19:41:39 +0300

    Week7Functions

---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿//total < 10 - 1%
+// total >= 10 && total < 20 - 5%
+// total >= 20 - 10&
+
+Console.WriteLine("Provide total:");
+int USERiNPUT = Int32.Parse(Console.ReadLine());
+
+double discount = CalculateDoscount(USERiNPUT);
+
+Console.WriteLine($"Your discount is: {discount}%");
+
+double newTotal = CalculateNewTotal(USERiNPUT, discount);
+
+Console.WriteLine($"Your updated total with {discount}%: {newTotal}");
+
+
+static double CalculateDoscount(int total)
+{
+    if (total < 10)
+    {
+        return 1;
+    }
+    else if (total >= 10 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+}
